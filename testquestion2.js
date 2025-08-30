9f0051772f55aea47351d0f2f96c633a6fe788e5
Emily Wong <emily.wongjq@gmail.com>
2025-08-30 06:36:52 +0000

    Aug 30, 2025, 6:36 AM

--- testquestion2.js
+++ testquestion2.js
@@ -5,7 +5,7 @@ function setup() {
 function draw() {
     for(let i = 1; i < 10; i++){
         fill(i* 50);
-        circle(50 + i *50, 100, 40)
+        circle(50 + i *50, 50 + i *50, 40);
     }
 }
 
